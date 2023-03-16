9a70dc2479173af58bc0379ca6202fa942dc33d7
Enzo Moreno <enzomoreno@EnzosComputer>
2023-03-16 15:16:46 -0400

    Playing and learning about maps.

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,52 +1,75 @@
-/* 
-async: this keyword defines that the function is
-asynchronous, put it just before it's body.
-*/
-void main() async {
-  /* 
-  Asynchronous code: lets your program complete work while
-  waiting for another operation to finish. Once initiated, an
-  asynchronous operation allows other operations to execute
-  before it completes. Examples of async applications are:
-  -> Fetching data over the network;
-  -> Writing/Reading to a database;
-  -> Creating a timer in your app;
-  */
+void main() {
+  final dictionary = {
+    'love': 'an intense feeling of deep affection I have 4 my wife',
+    'hate': 'feel intense or passionate dislike'
+  };
+
+  // retrieving info
+  final definition = dictionary['love'];
+  print(definition);
+
+  // adding a key : value
+  dictionary['dart'] = 'a incredible programming language';
+  print(dictionary['dart']);
+
+  // initilializing a map
+  var dict = Map(); // This is called a literal
+  var dictTwo = <String, String>{}; // using collection literals
 
   /* 
-  await: this keywork makes the program wait for the future
-  to be completed to proceed. It only waits INSIDE
-  ASYNC FUNCTIONS.
+  Using the Map literal + KEY, VALUE type of the map. (Map<X, Y>();)
   */
-  await fetchNews();
-  print('Fetching news');
-  myFunction();
-}
+  var dictThree = Map<String, String>();
 
-/* 
-Future: simply represents the result of an async operation in
-dart. It can have two states:
-* Incomplete:
-  The async operation is still running, which means that it
-  will return an Incomplete future.
-* Complete:
-  The async operation has succeeded and the future completes
-  with a value/error.
-    -> Value: if it succeeds
-    -> Error: if it fails
-*/
+  /* 
+  If we wanted to have multiple values with different types
+  we could use the keyword 'dynamic'.
+  */
+  var dictFour = <String, dynamic>{};
 
-Future<void> fetchNews() {
-  // Imagine that this function is fetching the news
-  // It has a delay of 2 seconds to represent the fetching
-  return Future.delayed(
-      const Duration(seconds: 2), () => print('News fetched'));
-}
+  /* 
+  Maps can also be initialized using empty curly braces.
+  */
+  var dictFive = {};
 
-Future<List<num>> fetchNewsTwo() {
-  return Future.delayed(Duration(seconds: 2), () => [1, 2, 3]);
-}
+  /* 
+  Deleting a value inside a map.
+  */
+  dictionary.remove('hate');
+  print(dictionary);
 
-void myFunction() async {
-  await fetchNews();
+  /* 
+  If you are unsure if a certain key exists inside the map, you
+  can check it out.
+  In a similar way you can use a method to check if a value exists
+  inside the map.
+  */
+  final isHatePresent = dictionary.containsKey('hate');
+  final equivalents = <String, int>{};
+  equivalents.addAll({'Dart': 3, 'Flutter': 5});
+  print(isHatePresent);
+  print(equivalents.containsValue(3));
+
+  /* 
+  Iterating over all of the Map entries: you can use a method
+  called 'forEach', which gives you the current key and value during
+  the iteration.
+  */
+
+  equivalents.forEach((key, value) {
+    print('$key means $value');
+  });
+
+  /* 
+  Maps are great to model objects and sending data across the
+  network too.
+  */
+  final user = <String, dynamic>{
+    'name': 'Richard',
+    'age': 25,
+    'parents': ['Livia', 'Rodrigo']
+  };
+  user.forEach((key, value) {
+    print('$key: $value');
+  });
 }

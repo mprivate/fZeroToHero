5232abad32f801c75cc4366fb0f544de6795ea5e
Enzo Moreno <enzomoreno@EnzosComputer>
2023-03-19 00:13:24 -0400

    Change on main

--- playground/test_application/lib/main.dart
+++ playground/test_application/lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('This is a test!')),
-        body: const Center(
-          child: Text('Hello people.'),
-        ),
+        appBar: AppBar(
+            title: const Text('Reaver'),
+            centerTitle: true,
+            actions: <Widget>[
+              IconButton(
+                iconSize: 40,
+                icon: Icon(Icons.wifi),
+                onPressed: () {
+                  print('null');
+                },
+              ),
+            ]),
       ),
     );
   }
